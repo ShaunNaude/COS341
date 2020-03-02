5652a147025f854ea7aa31af0ad6ee53d09fbb2c
Matthew <kershawmd@outlook.com>
2020-03-02 11:49:16 +0200

    Added functions

--- include/lexer/lexer.hpp
+++ include/lexer/lexer.hpp
@@ -5,4 +5,13 @@ class lexer {
 public:
     lexer();
     ~lexer();
+    void start(string);
+private:
+    int isOperator(string, int);
+    int isKeyword(string, int);
+    int isString(string, int);
+    int isVar(string, int);
+    int isInt(string, int);
+    
+    void AddNode(string, int, int);
 };
