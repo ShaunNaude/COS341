1b19d7c49398dcfdc2ff8d937aa01fcbf4671d98
Matthew <kershawmd@outlook.com>
2020-05-05 23:51:42 +0200

    Starting rule type checking

--- src/syntaxAnalysis/typeAnalysis.cpp
+++ src/syntaxAnalysis/typeAnalysis.cpp
@@ -29,7 +29,126 @@ void typeAnalysis::errorPrint(){//function to display errors
 }
 
 bool typeAnalysis::ruleValidate(){
+    bool ret = false;
+    vector< shared_ptr<SyntaxTree::node> > open;
+    open.push_back(Tree->root);
+    shared_ptr<SyntaxTree::node> temp;
+    string procName="";
 
+    while(!open.empty()){
+        temp=open.back();
+        open.pop_back();
+
+        //CODE
+        if(temp->name=="PROC"){
+            
+        }else
+        if(temp->name=="DECL"){
+            if(temp->children[0]->name=="NAME"){
+                
+            }else if(temp->children[0]->name=="DECL"){
+                
+            }
+        }else
+        //IO/CALLS
+        if(temp->name=="IO"){
+            
+        }else
+        if(temp->name=="CALL"){
+            
+        }else
+        //TYPE
+        if(temp->name=="TYPE"){
+            if(temp->children[0]->name=="num"){
+                
+            }else if(temp->children[0]->name=="string"){
+                
+            }else if(temp->children[0]->name=="bool"){
+                
+            }
+        }else
+        //ASSIGN
+        if(temp->name=="NAME"){
+            
+        }else if(temp->name=="VAR"){
+            
+        }else if(temp->name=="ASSIGN"){
+            if(temp->children[0]->name=="stringLiteral"){
+
+            }else if(temp->children[0]->name=="VAR"){
+
+            }else if(temp->children[0]->name=="NUMEXPR"){
+
+            }else if(temp->children[0]->name=="BOOL"){
+
+            }
+        }else
+        //NUMEXPR
+        if(temp->name=="NUMEXPR"){
+            if(temp->children[temp->children.size()-1]->name=="VAR"){
+                
+            }else if(temp->children[temp->children.size()-1]->name=="integerLiteral"){
+                
+            }else if(temp->children[temp->children.size()-1]->name=="CALC"){
+                
+            }
+        }else
+        //CALC
+        if(temp->name=="CALC"){
+            if(temp->children[temp->children.size()-1]->name=="add"){
+                
+            }else if(temp->children[temp->children.size()-1]->name=="sub"){
+                
+            }else if(temp->children[temp->children.size()-1]->name=="mult"){
+                
+            }
+        }else
+        //COND BRANCH
+        if(temp->name=="COND_BRANCH"){
+            if(temp->children[temp->children.size()-1]->name=="if"){
+                
+            }
+        }else
+        //BOOL
+        if(temp->name=="BOOL"){
+            if(temp->children[temp->children.size()-1]->name=="eq"){
+                
+            }else if(temp->children[temp->children.size()-1]->name=="not"){
+
+            }else if(temp->children[temp->children.size()-1]->name=="and"){
+
+            }else if(temp->children[temp->children.size()-1]->name=="or"){
+
+            }else if(temp->children[temp->children.size()-1]->name=="T"){
+
+            }else if(temp->children[temp->children.size()-1]->name=="F"){
+
+            }else if(temp->children.size()==3){
+                if(temp->children[1]->name=="<"){
+
+                }else if(temp->children[1]->name==">"){
+
+                }
+            }else if(temp->children.size()==1){
+                if(temp->children[0]->name=="VAR"){
+                
+                }
+            }
+        }else
+        //COND LOOP
+        if(temp->name=="COND_LOOP"){
+            if(temp->children[temp->children.size()-1]->name=="while"){
+
+            }else if(temp->children[temp->children.size()-1]->name=="for"){
+
+            }
+        }
+
+        for(int it = temp->children.size()-1; it > -1; it--)
+            open.push_back(temp->children[it]);
+    }
+
+    return ret;
 }
 
 bool typeAnalysis::hasValue(){
@@ -48,7 +167,7 @@ bool typeAnalysis::hasValue(){
                 if(temp->children[it]->name=="userDefined"){
                     procName=temp->children[it]->children[0]->name;
                     if(temp->children[it]->children[0]->tableNode->varibleID=="U"){
-                        cout<<"The procedure "+procName+" is undefined";
+                        cout<<"The procedure "+procName+" is undefined"<<endl;
                         ret = true;
                     }
                 }
@@ -59,7 +178,18 @@ bool typeAnalysis::hasValue(){
             for(int it = temp->children.size()-1; it > -1; it--){
                 if(temp->children[it]->name=="userDefined"){
                     if(temp->children[it]->children[0]->tableNode->varibleID=="U"){
-                        cout<<"In the procedure "+procName+" the variable "+temp->children[it]->children[0]->name+" is undefined";
+                        cout<<"In the procedure "+procName+" the variable "+temp->children[it]->children[0]->name+" is undefined"<<endl;
+                        ret = true;
+                    }
+                }
+            }
+        }
+
+        if(temp->name=="NAME"){
+            for(int it = temp->children.size()-1; it > -1; it--){
+                if(temp->children[it]->name=="userDefined"){
+                    if(temp->children[it]->children[0]->tableNode->varibleID=="U"){
+                        cout<<"In the procedure "+procName+" the variable "+temp->children[it]->children[0]->name+" is undefined"<<endl;
                         ret = true;
                     }
                 }
@@ -70,7 +200,7 @@ bool typeAnalysis::hasValue(){
             for(int it = temp->children.size()-1; it > -1; it--){
                 if(temp->children[it]->name=="userDefined"){
                     if(temp->children[it]->children[0]->tableNode->varibleID=="U"){
-                        cout<<"In the procedure "+procName+" the call to "+temp->children[it]->children[0]->name+" is undefined";
+                        cout<<"In the procedure "+procName+" the call to "+temp->children[it]->children[0]->name+" is undefined"<<endl;
                         ret = true;
                     }
                 }
